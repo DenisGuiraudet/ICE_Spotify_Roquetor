1c16ebf733abb30119c6c4a9a69e8b4ef159e517
Denis <guiraudet@live.fr>
2019-02-22 14:35:25 +0100

    init all pages

--- iceroquetor/src/app/app-routing.module.ts
+++ iceroquetor/src/app/app-routing.module.ts
@@ -1,15 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AlbumComponent } from './album/album.component';
 import { ArtistComponent } from './artist/artist.component';
+import { PlaylistComponent } from './playlist/playlist.component';
+import { TrackComponent } from './track/track.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '',
-    redirectTo: '/artist',
-    pathMatch: 'full'
+  {
+    path: '',
+    component: AppComponent
+  },
+  {
+    path: 'album',
+    component: AlbumComponent
   },
   {
     path: 'artist',
     component: ArtistComponent
+  },
+  {
+    path: 'playlist',
+    component: PlaylistComponent
+  },
+  {
+    path: 'track',
+    component: TrackComponent
   }
 ];
 
--- iceroquetor/src/app/app.module.ts
+++ iceroquetor/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArtistComponent } from './artist/artist.component';
+import { TrackComponent } from './track/track.component';
+import { AlbumComponent } from './album/album.component';
+import { PlaylistComponent } from './playlist/playlist.component';
+import { SearchbarComponent } from './ui/searchbar/searchbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArtistComponent
+    ArtistComponent,
+    TrackComponent,
+    AlbumComponent,
+    PlaylistComponent,
+    SearchbarComponent
   ],
   imports: [
     BrowserModule,
