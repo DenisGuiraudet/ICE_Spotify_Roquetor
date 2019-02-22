09bbae2fd41cf822aebe4e739eacc0dab97c183f
AlexandrePause <pausealexandre@gmail.com>
2019-02-22 11:44:21 +0100

    Init the project front with angular 7 and angular material

---
+++ iceroquetor/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ArtistComponent } from './artist/artist.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ArtistComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
