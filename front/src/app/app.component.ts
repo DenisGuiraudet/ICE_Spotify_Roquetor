6a2b2d7d86d6f971964317a72de6654dd38bd905
Denis <guiraudet@live.fr>
2019-06-14 10:11:54 +0200

    Moved the search to the search bar

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
 import { StoreService } from './store.service';
-import axios from 'axios';
-import { parseString } from 'xml2js';
-import { TYPES, SPOTIFY_TYPES } from './constants';
-import helpers from './helpers';
 
 @Component({
   selector: 'app-root',
@@ -13,79 +9,6 @@ import helpers from './helpers';
 export class AppComponent {
 
   constructor(private storeService: StoreService) {
-    // Spotify TOKEN
-    if (!this.storeService.spotifyUserToken) {
-      console.log('Spotify not init');
-    }
-    
-    // Back Call
-    axios.get('http://localhost:3232', {
-      params: {
-        token: this.storeService.spotifyUserToken,
-        value: "Michel le bresil",
-        type: TYPES.ARTIST
-      }
-    }).then(response => {
-      this.readXml();
-    }).catch(error => {
-      console.log(error);
-    }).then(() => {
-      this.readXml(); // TODO: remove from here once it works
-    });
-  }
-
-  readXml() {
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", "../assets/teste.xmi", false);
-    rawFile.onreadystatechange = () => {
-        if(rawFile.readyState === 4){
-            if(rawFile.status === 200 || rawFile.status == 0){
-                parseString(rawFile.responseText, (err, result) => {
-                  this.parsexml(result);
-                });
-            }
-        }
-    }
-    rawFile.send(null);
-  }
-
-  parsexml(xml) {
-    let datas = xml['spotify_Requetor:CommandManager'].request;
-    let mainData = datas[0].$;
-    let mainDataType = mainData['xsi:type'];
-
-    switch (mainDataType) {
-      case SPOTIFY_TYPES.ALBUM:
-        if (typeof mainData.track !== 'undefined') {
-          this.storeService.data = helpers.parseAlbum(datas);
-        } else {
-          this.storeService.data = helpers.parseAlbums(datas);
-        }
-        break;
-      case SPOTIFY_TYPES.ARTIST:
-          if (datas.length === 1) {
-            this.storeService.data = helpers.parseArtist(datas);
-          } else {
-            this.storeService.data = helpers.parseArtists(datas);
-          }
-        break;
-      case SPOTIFY_TYPES.PLAYLIST:
-          if (typeof mainData.track !== 'undefined') {
-            this.storeService.data = helpers.parsePlaylist(datas);
-          } else {
-            this.storeService.data = helpers.parsePlaylists(datas);
-          }
-        break;
-      case SPOTIFY_TYPES.TRACK:
-          if (datas.length === 1) {
-            this.storeService.data = helpers.parseTrack(datas);
-          } else {
-            this.storeService.data = helpers.parseTracks(datas);
-          }
-        break;
-      default:
-        console.warn('Type unknown', mainDataType);
-    }
   }
 }
 
--- front/src/app/ui/searchbar/searchbar.component.ts
+++ front/src/app/ui/searchbar/searchbar.component.ts
@@ -2,6 +2,11 @@ import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
+import { StoreService } from '../../store.service';
+import axios from 'axios';
+import { parseString } from 'xml2js';
+import { TYPES, SPOTIFY_TYPES } from '../../constants';
+import helperParser from '../../helpers/parser';
 
 export interface StateGroup {
   letter: string;
@@ -35,7 +40,7 @@ export class SearchbarComponent implements OnInit {
 
   stateGroupOptions: Observable<StateGroup[]>;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private storeService: StoreService) {}
 
   ngOnInit() {
     this.stateGroupOptions = this.stateForm.get('stateGroup')!.valueChanges
@@ -55,23 +60,108 @@ export class SearchbarComponent implements OnInit {
     return this.stateGroups;
   }
 
-  private cleaningRequest(request): string{
-    var words = request.split(' ');
-    let reqClean = "";
-    reqClean = words[1] + "/";
-    for(let i = 2; i < words.length; i++){
-      reqClean += words[i];
-      if(i != words.length-1)
-      reqClean += " "
-    }
-    if(words[0] == "search")
-    reqClean += "/only"
-    return reqClean;
-  }
   private search() {
-    let req = this.cleaningRequest(this.stateForm.get('stateGroup').value);
+    if (!this.storeService.spotifyUserToken) {
+      console.warn("No spotify token");
+      return;
+    }
+
+    let params = this.cleaningRequest(this.stateForm.get('stateGroup').value);
+    if (!params) {
+      console.warn("Missing some words in the request");
+      return;
+    }
     
-    console.log(req);
+    // Back Call
+    axios.get('http://127.0.0.1:9000', {
+      params
+    }).then(response => {
+      console.log('response', response);
+      this.readXml();
+    }).catch(error => {
+      console.log('error', error);
+    }).finally(() => {
+      this.readXml(); // TODO: remove from here once it works
+    });
+  }
+
+  private cleaningRequest(request): object {
+    let words = request.split(' ');
+    if (words.length < 3) {
+      return null;
+    }
+
+    let value = "";
+    for(let i = 2; i < words.length; i++) {
+      value += words[i];
+      if(i != words.length-1)
+      value += " ";
+    }
+
+    let params = {
+      token: this.storeService.spotifyUserToken,
+      value,
+      type: words[1]
+    };
+    if(words[0] == 'show') {
+      params['only'] = 'only';
+    }
+
+    return params;
+  }
+
+  readXml() {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", "../assets/teste.xmi", false);
+    rawFile.onreadystatechange = () => {
+        if(rawFile.readyState === 4){
+            if(rawFile.status === 200 || rawFile.status == 0){
+                parseString(rawFile.responseText, (err, result) => {
+                  this.parsexml(result);
+                });
+            }
+        }
+    }
+    rawFile.send(null);
+  }
+
+  parsexml(xml) {
+    let datas = xml['spotify_Requetor:CommandManager'].request;
+    let mainData = datas[0].$;
+    let mainDataType = mainData['xsi:type'];
+
+    switch (mainDataType) {
+      case SPOTIFY_TYPES.ALBUM:
+        if (typeof mainData.track !== 'undefined') {
+          this.storeService.data = helperParser.parseAlbum(datas);
+        } else {
+          this.storeService.data = helperParser.parseAlbums(datas);
+        }
+        break;
+      case SPOTIFY_TYPES.ARTIST:
+          if (datas.length === 1) {
+            this.storeService.data = helperParser.parseArtist(datas);
+          } else {
+            this.storeService.data = helperParser.parseArtists(datas);
+          }
+        break;
+      case SPOTIFY_TYPES.PLAYLIST:
+          if (typeof mainData.track !== 'undefined') {
+            this.storeService.data = helperParser.parsePlaylist(datas);
+          } else {
+            this.storeService.data = helperParser.parsePlaylists(datas);
+          }
+        break;
+      case SPOTIFY_TYPES.TRACK:
+          if (datas.length === 1) {
+            this.storeService.data = helperParser.parseTrack(datas);
+          } else {
+            this.storeService.data = helperParser.parseTracks(datas);
+          }
+        break;
+      default:
+        console.warn('Type unknown', mainDataType);
+    }
   }
 
 }
