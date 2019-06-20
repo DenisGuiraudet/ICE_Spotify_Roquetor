7b0da86277fcbde8a714f0ec930da956a3f3dedd
Denis <guiraudet@live.fr>
2019-06-20 10:49:20 +0200

    Some good fake data

--- front/src/app/album/album.component.ts
+++ front/src/app/album/album.component.ts
@@ -10,30 +10,30 @@ export class AlbumComponent implements OnInit {
 
   data = {
     id: 'id',
-    image: 'https://assets.afcdn.com/story/20180420/1155990_w767h767c1cx1879cy955.jpg',
-    name: 'name',
-    popularity: 0,
-    release_date: 'release_date',
+    image: 'https://i.scdn.co/image/3d99e3949772e8cc079023c52d027c6a2cbfb14b',
+    name: 'Platinium',
+    popularity: 12402,
+    release_date: '10/12/12',
     release_date_precision: 'release_date_precision',
     tracks: [
       {
         duration_ms: 0,
         id: 'id',
-        name: 'name',
+        name: 'Say hi',
         track_number: 1,
         danceability: 0.735
       },
       {
         duration_ms: 12,
         id: '1212',
-        name: 'Une chanson',
+        name: 'Platinium',
         track_number: 2,
-        danceability: 0.1
+        danceability: 0.11
       },
       {
         duration_ms: 122,
         id: '444',
-        name: 'Toto',
+        name: 'Hello darkness',
         track_number: 3,
         danceability: 0.99
       }
